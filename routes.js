f71b97d3ea2f9e5d80d31c71846c3f728bb4a8f9
Lucas Pedroso <lucassppedroso@gmail.com>
2020-07-16 12:35:31 -0300

    Criação das rotas Edit e Delete, criação de efeitos hover dos botões

--- routes.js
+++ routes.js
@@ -20,6 +20,8 @@ routes.get('/teachers/:id/edit', teachers.edit);
 
 routes.post('/teachers', teachers.post);
 
+routes.put('/teachers', teachers.put);
 
+routes.delete('/teachers', teachers.delete);
 
 module.exports = routes;
